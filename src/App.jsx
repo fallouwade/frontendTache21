de59d2aaf064cbc7ed77f723f80c28bca960d54b
lenovo <wwadefallou@gmail.com>
2025-01-20 09:59:35 +0100

    Intégration page client

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,16 @@
 import React from 'react';
-import Navbar from './Composants/PageClient/NavbarAside/NavBar';
-import Sidebar from './Composants/PageClient/NavbarAside/SIdebar';
 import  { useState } from 'react';
+import Utilisateur from './Composants/PageClient/Pages/Utilisateur';
+
 
 function App() {
-  const [isSidebarOpen, setSidebarOpen] = useState(false);
-
-  const toggleSidebar = () => {
-    setSidebarOpen((prev) => !prev);
-  };
+ 
   return (
-    <div className="flex">
-    {/* Navbar */}
-    <Navbar toggleSidebar={toggleSidebar} />
-
-    {/* Sidebar */}
-    <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
-
-    {/* Main Content */}
-    <div className="flex-1 pt-16 p-4 bg-gray-100">
-      <h1 className="text-2xl font-bold">Bienvenue sur notre plateforme</h1>
-      <p className="text-gray-600 mt-4">
-        Recherchez des professionnels qualifiés, réservez leurs services, et payez en ligne.
-      </p>
+    <div >
+   
+   
+    <div >
+      <Utilisateur/>
     </div>
   </div>
   );
