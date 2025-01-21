7748f7e9259046cecc2f7deea93b1b0c55ef47f8
Ambdouroihmane Soilihi <ambdousoilihiit@gmail.com>
2025-01-21 11:43:32 +0000

    commit for PR

--- src/Composants/adminComposants/NavbarAside/Sidebar.jsx
+++ src/Composants/adminComposants/NavbarAside/Sidebar.jsx
@@ -1,11 +1,10 @@
 import { useState, useEffect } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-// import { CgComment } from "react-icons/cg";
-import { FaUsersGear } from "react-icons/fa6";
-import { FaUserTie } from "react-icons/fa6";
+import { FaUsersGear, FaUserTie } from "react-icons/fa6";
 import { RxDashboard } from "react-icons/rx";
-import { MdHomeRepairService } from "react-icons/md";
-// import { BiSolidCategoryAlt } from "react-icons/bi";
+import { BiSolidCategoryAlt } from "react-icons/bi";
+// import { MdHomeRepairService } from "react-icons/md";
+// import { CgComment } from "react-icons/cg";
 
 const Sidebar = ({ isOpen }) => {
   const location = useLocation();
@@ -31,9 +30,9 @@ const Sidebar = ({ isOpen }) => {
     { id: 'dashboard', icon: <RxDashboard />, label: 'Dashboard', route: '/' },
     { id: 'prestataires', icon: <FaUserTie />, label: 'Prestataires', route: '/prestataire' },
     { id: 'clients', icon: <FaUsersGear />, label: 'Clients', route: '/clients' },
-    { id: 'services', icon: <MdHomeRepairService />, label: 'Services', route: '/services' },
+    // { id: 'services', icon: <MdHomeRepairService />, label: 'Services', route: '/services' },
     // { id: 'avis', icon: <CgComment />, label: 'Avis', route: '/avis' },
-    // { id: 'categories', icon: <BiSolidCategoryAlt />, label: 'Catégories', route: '/categories' }
+    { id: 'categories', icon: <BiSolidCategoryAlt />, label: 'Catégories', route: '/categories' }
   ];
 
   return (
