1791fb647430068c72686818813ea8fba51ddbe0
Papa Abdoulaye Diouf <papalayediouf17@gmail.com>
2025-01-23 08:59:56 +0000

    test front 21

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,8 @@
-
 import { Route, Routes } from "react-router-dom";
 
-import Communautaire from './Pages/Communautaire.jsx';
-import Client from './Pages/Client.jsx';
-import Connection from './Authentification/PagesConnexion/Connection.jsx'
+import Communautaire from "./Pages/Communautaire.jsx";
+import Client from "./Pages/Client.jsx";
+import Connection from "./Authentification/PagesConnexion/Connection.jsx";
 import Inscription from "./Authentification/InscriptionPrestataire/Inscription.jsx";
 import MotDePasseOublie from "./Authentification/PagesPassword/MotDePasseOublie.jsx";
 import Signup from "./Authentification/InscriptionClient/Signup.jsx";
@@ -12,14 +11,9 @@ import Categorie from "./Pages/PageAdmin/categorie/Categorie.jsx";
 import InfoClients from "./Pages/PageAdmin/clients/InfoClients.jsx";
 import { DashboardAdmin } from "./Pages/PageAdmin/dashboard/DashboardAdmin.jsx";
 
-
-
-
 function App() {
   return (
-
     <Routes>
-
       <Route path="/" element={<Communautaire />} />
 
       <Route path="/connexion" element={<Connection />} />
@@ -32,7 +26,6 @@ function App() {
       {/* Inscription Client */}
       <Route path="/signup" element={<Signup />} />
 
-
       <Route path="/Client" element={<Client />} />
 
       <Route path="/categorie" element={<Categorie />} />
@@ -40,17 +33,8 @@ function App() {
       <Route path="/infoClient" element={<InfoClients />} />
 
       <Route path="/dashboardAdmin" element={<DashboardAdmin />} />
-
-     
-
-
-
-
     </Routes>
-
-   
-  )
-
+  );
 }
 
-export default App
+export default App;
