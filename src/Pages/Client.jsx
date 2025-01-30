510de69785be4d2e2d4d30ec10aa1d5f401af453
lenovo <wwadefallou@gmail.com>
2025-01-30 16:02:36 +0100

    test

--- src/Pages/Client.jsx
+++ src/Pages/Client.jsx
@@ -1,37 +1,13 @@
+import React, { useState } from "react";
+import { useLocation } from "react-router-dom";
 import LayoutClients from "./PageClient/layout/LayoutClients";
-import { Outlet, useLocation } from "react-router-dom";
 import ServiceGrid from "./PageClient/Components/ServiceGrid";
-import { useState } from "react";
 
 function Client() {
-   const location = useLocation();
-    const [filters, setFilters] = useState({
-      showFilters: false,
-      category: "",
-      locality: "",
-      sortBy: "popular",
-      currentPage: 1,
-      searchQuery: "",
-      showFavorites: false,
-    });
-
-  const handleFilterChange = (key, value) =>
-    setFilters((prev) => ({ ...prev, [key]: value }));
-
+ 
   return (
-    <LayoutClients handleFilterChange={handleFilterChange}>
-
-
-{location.pathname === "/Client" ? (
-        <ServiceGrid 
-          {...filters} 
-          setCurrentPage={(page) => handleFilterChange("currentPage", page)} 
-        />
-      ) : (
-        <Outlet />
-      )}
-
-=
+    <LayoutClients>
+   
     </LayoutClients>
   );
 }
