a6f7d0291c860e42b99e15fc49408d675bb1c694
alimatou sow <a.s33@zig.univ.sn>
2025-01-30 10:15:58 +0000

    afffichage

--- src/pages/Profil.jsx
+++ src/pages/Profil.jsx
@@ -5,33 +5,41 @@ import Image from '/images/electricien.jpg'
 import { Link } from "react-router-dom";
 
 const Profil = () => {
-  const [userData, setUserData] = useState({
-    nom: '',
-    prenom: '',
-    email: '',
-    telephone: '',
-    description: '',
-    nomEntreprise: ''
-  })
+  const [formData, setFormData] = useState({})
+  const token = JSON.parse(localStorage.getItems())
   const [isLoading, setIsLoading] = useState(true)
   const [error, setError] = useState(null)
 
   useEffect(() => {
+    // fetch("https://backendtache21.onrender.com/api/prestataires/profil-prestataire", {
+    //   method: 'GET',
+    // })
+    // .then((res) => res.json())
+    // .then((data) => {
+    //   console.log(data, "formData");
+    //   setFormData(data.data);
+    // })
     const fetchPrestataireData = async () =>{
       try {
         const response = await axios.get("https://backendtache21.onrender.com/api/prestataires/profil-prestataire", {
+          nom: formData.nom,
+          prenom: formData.nom,
+          email: formData.nom,
+          telephone: formData.nom,
+          
+        },{
           headers: { "Content-Type": "application/json" },
           body: JSON.stringify(get)
         })
         const data = await response.json()
         setUserData(data)
-        // setIsLoading(false)
+        setIsLoading(false)
         .then(() => {
           Navigate('/profil')
         });
       } catch (error) {
-        // setError(error.response ? error.response.data : "Une erreur est survenue")
-        // setIsLoading(false)
+        setError(error.response ? error.response.data : "Une erreur est survenue")
+        setIsLoading(false)
       }
     };
     fetchPrestataireData()
@@ -62,27 +70,27 @@ const Profil = () => {
           <div className="grid grid-cols-2 ">
             <div className="mb-3">
               <p className="font-semibold text-gray-600">Nom</p>
-              <p className="text-gray-800 flex flex-wrap">{userData.nom}</p>
+              <p className="text-gray-800 flex flex-wrap">{formData.nom}</p>
             </div>
             <div>
               <p className="font-semibold text-gray-600">Prénom</p>
-              <p className="text-gray-800">{userData.prenom}</p>
+              <p className="text-gray-800">{formData.prenom}</p>
             </div>
             <div>
               <p className="font-semibold text-gray-600">Téléphone</p>
-              <p className="text-gray-800">{userData.telephone}</p>
+              <p className="text-gray-800">{formData.telephone}</p>
             </div>
             <div>
               <p className="font-semibold text-gray-600">Email</p>
-              <p className="text-gray-800">{userData.email}</p>
+              <p className="text-gray-800">{formData.email}</p>
             </div>
           </div>
         </div>
         <div className="bg-white rounded-lg shadow-md p-6">
           <img src={Image} alt="Profile" className="w-32 h-32 rounded-full mx-auto mb-4" />
-          <h2 className="text-xl font-semibold text-center mb-2">{userData.prenom}</h2>
-          <p className="text-gray-600 text-center mb-4">{userData.nomEntreprise}</p>
-          <p className="text-center mb-4">{userData.description}</p>
+          <h2 className="text-xl font-semibold text-center mb-2">{formData.prenom}</h2>
+          <p className="text-gray-600 text-center mb-4">{formData.nomDeLEntreprise}</p>
+          <p className="text-center mb-4">{formData.description}</p>
           <Link
             to="/editerprofil"
             className="block w-full text-center bg-purple-600 text-white py-2 px-4 rounded hover:bg-purple-700"
