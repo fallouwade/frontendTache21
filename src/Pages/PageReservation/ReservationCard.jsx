b26bb821b840e92dab6bbb3725ec7fb0c180335e
lenovo <wwadefallou@gmail.com>
2025-02-02 14:04:33 +0100

    Test3

--- src/Pages/PageReservation/ReservationCard.jsx
+++ src/Pages/PageReservation/ReservationCard.jsx
@@ -82,7 +82,7 @@ const ReservationCard = ({ selectedDate, id }) => {
 
       // Envoi de la requête POST au serveur
       const response = await axios.post(
-        'https://backendtache21.onrender.com/api/demandes-services/demande',
+        ' http://localhost:5000/api/demandes-services/demande',
         demandeService,
         {
           headers: {
