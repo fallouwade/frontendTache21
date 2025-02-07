c3e45a3c4e8248a73748295d155d21caf7649969
ndeye ngone gningue <gningngone50@gmail.com>
2025-02-07 11:15:39 +0000

    pagecommunaitaire8

--- src/Pages/PageReservation/DetailsPrestataire.jsx
+++ src/Pages/PageReservation/DetailsPrestataire.jsx
@@ -18,17 +18,6 @@ const DetailsPrestataire = (props) => {
 
   return (
     <div className="max-w-7xl mx-auto p-4">
-      {/* Header */}
-      <div className="space-y-4 mb-6">
-        <h1 className="text-2xl font-semibold">Service {} d'urgence 24h/24 - Dakar et environs</h1>
-        <div className="flex items-center gap-2 text-sm">
-          <span>Intervention express</span>
-          <span>•</span>
-          <span>Intervention 7j/7</span>
-          <span>•</span>
-          <span>Devis gratuit </span>
-        </div>
-      </div>
    
       <div className="grid md:grid-cols-3 gap-8">
         <div className="md:col-span-2 space-y-6">
