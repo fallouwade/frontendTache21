2fe30c76ba51a30dad6deb2337ac5347c5959fbf
lenovo <wwadefallou@gmail.com>
2025-02-16 14:37:48 +0100

    jour

--- src/Authentification/inscription/InscriptionPrestataire.jsx
+++ src/Authentification/inscription/InscriptionPrestataire.jsx
@@ -154,7 +154,7 @@ const InscriptionPrestataire = () => {
                   onChange={handleChange}
                   className="w-full p-2 mt-2 border border-gray-300 rounded-xl"
                   required
-                  readOnly
+                 
                 />
               </div>
               <div className="mb-2 flex-1">
@@ -172,7 +172,7 @@ const InscriptionPrestataire = () => {
                   onChange={handleChange}
                   className="w-full p-2 mt-2 border border-gray-300 rounded-xl"
                   required
-                  readOnly
+                 
                 />
               </div>
             </div>
@@ -192,7 +192,7 @@ const InscriptionPrestataire = () => {
                   onChange={handleChange}
                   className="w-full p-2 mt-2 border border-gray-300 rounded-xl"
                   required
-                  readOnly
+                 
                 />
               </div>
               <div className="mb-2 flex-1">
