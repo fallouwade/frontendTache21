1507640ee12e95c58ef43ab30062d9a465af55c2
Abdou Khoudoss <khoudossmbacke18@gmail.com>
2025-02-17 15:24:34 +0000

    favorits

--- src/Pages/PageClient/Components/FavoriteButton.jsx
+++ src/Pages/PageClient/Components/FavoriteButton.jsx
@@ -1,6 +1,8 @@
 import { FaHeart } from "react-icons/fa"
+import { useState } from "react"
 
 function FavoriteButton({ favorites = [], onToggleFavorite, showFavorites }) {
+  const [isHovered, setIsHovered] = useState(false)
   const favoritesCount = Array.isArray(favorites) ? favorites.length : 0
 
   return (
@@ -11,11 +13,20 @@ function FavoriteButton({ favorites = [], onToggleFavorite, showFavorites }) {
         }`}
         aria-label="Favoris"
         onClick={onToggleFavorite}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
       >
         <FaHeart className="h-6 w-6" />
       </button>
+
+      {isHovered && (
+        <div className="absolute left-1/2 transform -translate-x-1/2 -top-8 bg-gray-800 text-white text-xs px-2 py-1 rounded-md">
+          Favoris
+        </div>
+      )}
+
       {favoritesCount > 0 && (
-        <div className="absolute -top-1 -right-1 bg-gray-500 text-white text-xs md:w-5 md:h-5 w-5 h-2 flex items-center justify-center rounded-full font-medium">
+        <div className="absolute -top-1 -right-1 bg-gray-500 text-white text-xs md:w-5 md:h-5 w-5 h-5 flex items-center justify-center rounded-full font-medium">
           {favoritesCount}
         </div>
       )}
@@ -24,4 +35,3 @@ function FavoriteButton({ favorites = [], onToggleFavorite, showFavorites }) {
 }
 
 export default FavoriteButton
-
